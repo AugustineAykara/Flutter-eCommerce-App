fa74d2cf10c8fcb2a6924169ca6b684ac1bce358
Vivek <44610017+Vivek-KP@users.noreply.github.com>
2020-05-19 12:49:16 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoginPage(),
+      home: Flash(),
     );
   }
 }
